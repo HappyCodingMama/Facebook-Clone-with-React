a3245fb34d603d39c4cef2c39007b5c20af98cb3
HappyCodingMama <happycodingmama@gmail.com>
2021-02-17 22:18:28 +0900

    build the Widgets Component

--- facebook-clone/src/App.js
+++ facebook-clone/src/App.js
@@ -2,19 +2,27 @@ import React from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
+import Widgets from "./Widgets";
 import "./App.css";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <Header />
+      {!user ? (
+        <h1>Login</h1>
+      ) : (
+        <>
+          <Header />
 
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Feed */}
-        {/* Widgets */}
-      </div>
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- facebook-clone/src/Post.js
+++ facebook-clone/src/Post.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { Avatar } from "@material-ui/core";
+import {
+  ThumbUp,
+  ChatBubbleOutline,
+  NearMe,
+  AccountCircle,
+  ExpandMoreOutlined,
+} from "@material-ui/icons";
 import "./Post.css";
 
 function Post({ profilePic, image, username, timestamp, message }) {
@@ -17,6 +24,25 @@ function Post({ profilePic, image, username, timestamp, message }) {
       </div>
       <div className="post__image">
         <img src={image} alt="" />
+      </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUp />
+          <p>Like</p>
+        </div>
+        <div className="post__option">
+          <ChatBubbleOutline />
+          <p>Comment</p>
+        </div>
+        <div className="post__option">
+          <NearMe />
+          <p>Share</p>
+        </div>
+        <div className="post__option">
+          <AccountCircle />
+          <ExpandMoreOutlined />
+        </div>
       </div>
     </div>
   );
